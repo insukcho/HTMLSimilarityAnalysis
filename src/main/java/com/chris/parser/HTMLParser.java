0962c5d1c33353ad4298037eb9371dfc07e47b22
Insuk Cho (Chris) <isi.cho@gmail.com>
2016-08-20 11:19:23 +0900

    Change words data structure from List<String> to Map<String, Integer>

--- src/main/java/com/chris/parser/HTMLParser.java
+++ src/main/java/com/chris/parser/HTMLParser.java
@@ -1,35 +1,72 @@
 package com.chris.parser;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * For HTML parsing and generate words data for analysis.
+ * 
+ * {@link #getAllWordsInBody(Document)} takes Jsoup document and generate all
+ * words for analysis.
+ * 
+ * @author Chris
+ *
+ */
 public class HTMLParser {
-	
+
 	private final static String BODY_TAG_NAME = "body";
 	private final static String WORD_SEPERATOR = " ";
-	
+	private final static String EXCEPT_ALPHABET = "[^A-Za-z]";
+
+	/**
+	 * Get html page from given URL and parse to Jsoup document which has samme
+	 * domm structure.
+	 * 
+	 * @param url
+	 * @return Jsoup document
+	 * @throws IOException
+	 */
 	public static Document htmlParsing(String url) throws IOException {
 		return Jsoup.connect(url).get();
 	}
-	
-	public static List<String> getAllWordsInBody(Document doc) {
-		List<String> words = new ArrayList<String>();
-		
+
+	/**
+	 * Get all text in 'body' tags and split by {@link #WORD_SEPERATOR}. Remain
+	 * only alphabet in the word using regex. Finally, generate map, key is the
+	 * word and value is the count of word. This is important for analysis
+	 * efficiently.
+	 * 
+	 * @param doc
+	 * @return map with word and each count
+	 */
+	public static Map<String, Integer> getAllWordsInBody(Document doc) {
+		Map<String, Integer> wordNCount = new HashMap<>();
+
 		Elements elements = doc.getElementsByTag(BODY_TAG_NAME);
-		for(Element e: elements) {
-			if(e.hasText()) {
-				words.addAll(Arrays.asList(e.text().split(WORD_SEPERATOR)));
+		for (Element e : elements) {
+			if (e.hasText()) {
+				for (String word : e.text().split(WORD_SEPERATOR)) {
+					// remove all character which is not alphabet
+					word = word.replaceAll(EXCEPT_ALPHABET, "");
+					
+					if (wordNCount.containsKey(word)) {
+						// accumulate the count
+						wordNCount.put(word, wordNCount.get(word) + 1);
+					} else {
+						// initial input
+						wordNCount.put(word, 1);
+					}
+				}
 			}
 		}
-		
-		return words;
+
+		return wordNCount;
 	}
 
 }
--- src/test/java/com/chris/parser/HTMLParserTests.java
+++ src/test/java/com/chris/parser/HTMLParserTests.java
@@ -1,35 +1,32 @@
 package com.chris.parser;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.IsCollectionContaining.hasItem;
-import static org.junit.Assert.assertThat;
-
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HTMLParserTests {
-	private final static String TEST_URL = "https://www.google.com/";
-	
 	@Test
 	public void testHtmlParser() throws IOException {
-		Document doc = HTMLParser.htmlParsing(TEST_URL);
-		assertThat(doc.getAllElements().size(), equalTo(91));
+		Document doc = HTMLParser.htmlParsing("https://www.google.com/");
+		Assert.assertEquals("Google", doc.title());
 	}
-	
+
 	@Test
 	public void testGetAllWordsInBody() {
 		String html = "<html><head><title>First parse</title></head>"
-				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
-		
-		List<String> allWordsInBody = HTMLParser.getAllWordsInBody(Jsoup.parse(html));
-		assertThat(allWordsInBody, hasItem("Parsed"));
-		assertThat(allWordsInBody, hasItem("HTML"));
-		assertThat(allWordsInBody, hasItem("doc."));
+				+ "<body><p>Parsed HTML into a doc. HTML is markup language. </p></body></html>";
+
+		Map<String, Integer> wordNCount = HTMLParser.getAllWordsInBody(Jsoup.parse(html));
+		Assert.assertTrue(wordNCount.containsKey("Parsed"));
+		Assert.assertEquals(new Integer(1), wordNCount.get("Parsed"));
+		Assert.assertTrue(wordNCount.containsKey("HTML"));
+		Assert.assertEquals(new Integer(2), wordNCount.get("HTML"));
+		Assert.assertTrue(wordNCount.containsKey("doc"));
+		Assert.assertEquals(new Integer(1), wordNCount.get("doc"));
 	}
-	
-	
+
 }
