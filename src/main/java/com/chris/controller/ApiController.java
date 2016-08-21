3501728cc08b5e560a14063d18697f94b1a4ba07
Insuk Cho (Chris) <isi.cho@gmail.com>
2016-08-21 21:37:35 +0900

    Add APIs and refactoring
    -. add API to delete All articles
    -. add API to retrieve all processed articles from redis
    -. change api request param from topic to url

--- src/main/java/com/chris/controller/ApiController.java
+++ src/main/java/com/chris/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.chris.controller;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,24 +8,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chris.model.Article;
+import com.chris.repository.ArticleRepository;
+import com.chris.worker.Consumer;
 
 @RestController
 public class ApiController {
 
 	BlockingQueue<Article> requestQueue;
+	ArticleRepository articleRepository;
 	
-	public ApiController(BlockingQueue<Article> requestQueue) {
+	public ApiController(BlockingQueue<Article> requestQueue, ArticleRepository articleRepository) {
 		this.requestQueue = requestQueue;
+		this.articleRepository = articleRepository;
+		new Thread(new Consumer(requestQueue, articleRepository)).start();
 	}
 
 	@RequestMapping("/")
-	public String produceArticle(@RequestParam("topic") String topic) throws InterruptedException {
-		requestQueue.put(new Article(System.currentTimeMillis(), topic, System.nanoTime()));
+	public String produceArticle(@RequestParam("url") String url) throws InterruptedException {
+		requestQueue.put(new Article(System.currentTimeMillis(), url, System.nanoTime()));
 		return "Create and enqueu Article sucessfully!";
 	}
 
 	@RequestMapping("/request/articles")
 	public String getRequestArticles() {
 		return requestQueue.toString();
+	}
+	
+	@RequestMapping("/articles")
+	public List<Article> getProcessedArticles() throws Exception {
+		return articleRepository.findAll();
+	}
+	
+	@RequestMapping("/delete/articles")
+	public String deleteAllArticles() throws Exception {
+		articleRepository.deleteAll();
+		return "Delete all articles, successfully!!";
 	}
 }
